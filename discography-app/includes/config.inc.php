88fe8d1f5c932766b4658e0e28f42008c4dff1c5
mdbatruch <mdbatruch@gmail.com>
2017-08-24 10:39:44 -0400

    Update config.inc.php

--- discography-app/includes/config.inc.php
+++ discography-app/includes/config.inc.php
@@ -4,7 +4,7 @@
 
         define( 'DB_HOST',      'localhost' );
         define( 'DB_USER',      'n00363842' );
-        define( 'DB_PASSWORD',  'vaginal11' );
+        define( 'DB_PASSWORD',  'babyduck' );
         define( 'DB_NAME',      'humber-wddm' );
 
 define( 'REWRITE_URLS', true );
